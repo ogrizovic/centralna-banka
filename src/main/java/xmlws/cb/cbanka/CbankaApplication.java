3f06ab765da601aeb4e860e4d315a813be26c8e5
nikola3333 <nikolarakoc@yahoo.com>
2017-09-09 19:16:43 +0200

    centralna banka

---
+++ src/main/java/xmlws/cb/cbanka/CbankaApplication.java
@@ -0,0 +1,12 @@
+package xmlws.cb.cbanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbankaApplication.class, args);
+	}
+}
